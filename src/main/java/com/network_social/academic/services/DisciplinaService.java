40b409369568f2241fd4018d90e20821f88e8245
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-06-28 18:29:25 -0300

    criar e implementar service de disciplina

---
+++ src/main/java/com/network_social/academic/services/DisciplinaService.java
@@ -0,0 +1,45 @@
+package com.network_social.academic.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.network_social.academic.models.Disciplina;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DisciplinaService {
+
+    private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+    public List<Disciplina> findAll() {
+        return disciplinas;
+    }
+
+    public Disciplina insert(Disciplina disciplina) {
+        disciplina.setId(disciplinas.size());
+        disciplinas.add(disciplina);
+        return disciplina;
+    }
+
+    public Disciplina findById(int id) {
+        if (disciplinas.isEmpty() || id < 0 || id >= disciplinas.size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return disciplinas.get(id);
+    }
+
+    public Disciplina update(Disciplina disciplina) throws ArrayIndexOutOfBoundsException {
+        findById(disciplina.getId());
+        disciplinas.set(disciplina.getId(), disciplina);
+        return disciplina;
+
+    }
+
+    public Disciplina delete(int id) throws ArrayIndexOutOfBoundsException {
+        findById(id);
+        return disciplinas.remove(id);
+    }
+}
