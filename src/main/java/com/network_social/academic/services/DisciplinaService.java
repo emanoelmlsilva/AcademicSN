d2d90da0409044006ad498bf6e8ce80616126815
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-12 17:28:50 -0300

    adicoinar carregamento de dados do json

--- src/main/java/com/network_social/academic/services/DisciplinaService.java
+++ src/main/java/com/network_social/academic/services/DisciplinaService.java
@@ -1,45 +1,77 @@
 package com.network_social.academic.services;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
+import javax.annotation.PostConstruct;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.network_social.academic.models.Disciplina;
+import com.network_social.academic.repositories.DisciplinaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class DisciplinaService {
 
-    private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
+    public DisciplinaService() {
+    }
+
+    @PostConstruct
+    public void initDisciplinas() {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<Disciplina>> typeReference = new TypeReference<List<Disciplina>>() {
+        };
+        InputStream inputStream = ObjectMapper.class.getResourceAsStream("/json/disciplinas.json");
+        try {
+            System.out.println("inputStream " + inputStream);
+            System.out.println("mapper " + mapper);
+            List<Disciplina> teste = mapper.readValue(inputStream, typeReference);
+            // List<Disciplina> disciplinas = mapper.readValue(inputStream, typeReference);
+            System.out.println("passou 1");
+            this.disciplinaRepository.saveAll(teste);
+            System.out.println("passou 2");
+            System.out.println("Disciplinas salvas no BD!");
+        } catch (IOException erro) {
+            System.out.println("Não foi possivel salvar as disciplinas no BD!" + erro.getMessage());
+        }
+
+    }
 
     public List<Disciplina> findAll() {
-        return disciplinas;
+        return disciplinaRepository.findAll();
     }
 
-    public Disciplina insert(Disciplina disciplina) {
-        disciplina.setId(disciplinas.size());
-        disciplinas.add(disciplina);
-        return disciplina;
-    }
+    // public Disciplina insert(Disciplina disciplina) {
+    // disciplina.setId(null);
+    // disciplinaRepository.save(disciplina);
+    // return disciplina;
+    // }
 
-    public Disciplina findById(int id) {
-        if (disciplinas.isEmpty() || id < 0 || id >= disciplinas.size()) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        return disciplinas.get(id);
-    }
+    // public Disciplina findById(Long id) {
+    // Optional<Disciplina> disciplina = disciplinaRepository.findById(id);
+    // return disciplina;
+    // }
 
-    public Disciplina update(Disciplina disciplina) throws ArrayIndexOutOfBoundsException {
-        findById(disciplina.getId());
-        disciplinas.set(disciplina.getId(), disciplina);
-        return disciplina;
+    // public Disciplina update(Disciplina disciplina) throws
+    // ArrayIndexOutOfBoundsException {
+    // findById(disciplina.getId());
+    // disciplinas.set(disciplina.getId(), disciplina);
+    // return disciplina;
 
-    }
+    // }
 
-    public Disciplina delete(int id) throws ArrayIndexOutOfBoundsException {
-        findById(id);
-        return disciplinas.remove(id);
-    }
+    // public Disciplina delete(int id) throws ArrayIndexOutOfBoundsException {
+    // findById(id);
+    // return disciplinas.remove(id);
+    // }
 }
