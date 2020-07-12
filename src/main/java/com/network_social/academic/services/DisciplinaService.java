eef54d2eed9b8090925962cf1f9f02e086be148b
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-12 18:00:29 -0300

    adicionar verificação de discilina salvas

--- src/main/java/com/network_social/academic/services/DisciplinaService.java
+++ src/main/java/com/network_social/academic/services/DisciplinaService.java
@@ -3,8 +3,10 @@ package com.network_social.academic.services;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
@@ -33,15 +35,26 @@ public class DisciplinaService {
         };
         InputStream inputStream = ObjectMapper.class.getResourceAsStream("/json/disciplinas.json");
         try {
-            System.out.println("inputStream " + inputStream);
-            System.out.println("mapper " + mapper);
-            List<Disciplina> teste = mapper.readValue(inputStream, typeReference);
-            // List<Disciplina> disciplinas = mapper.readValue(inputStream, typeReference);
-            System.out.println("passou 1");
-            this.disciplinaRepository.saveAll(teste);
-            System.out.println("passou 2");
+            List<Disciplina> disciplinas = mapper.readValue(inputStream, typeReference);
+
+            List<Disciplina> bdDisciplinas = this.disciplinaRepository.findAll();
+            System.out.println("bdDisciplinas " + bdDisciplinas.size());
+
+            if (bdDisciplinas.size() > 0) {
+
+                List<Disciplina> newList = disciplinas.stream()
+                        .filter(bdDisciplina -> bdDisciplinas.stream()
+                                .anyMatch(disciplina -> !disciplina.getNome().equals(bdDisciplina.getNome())))
+                        .collect(Collectors.toList());
+                disciplinas = newList;
+            }
+
+            this.disciplinaRepository.saveAll(disciplinas);
             System.out.println("Disciplinas salvas no BD!");
-        } catch (IOException erro) {
+
+        } catch (
+
+        IOException erro) {
             System.out.println("Não foi possivel salvar as disciplinas no BD!" + erro.getMessage());
         }
 
