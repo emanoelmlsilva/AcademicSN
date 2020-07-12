f780557e12955cd462c08afa5004c56159058913
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-11 22:53:28 -0300

    adicionar criação de tabela do banco

--- src/main/java/com/network_social/academic/models/Disciplina.java
+++ src/main/java/com/network_social/academic/models/Disciplina.java
@@ -1,15 +1,21 @@
 package com.network_social.academic.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Disciplina implements Comparable<Disciplina> {
+public class Disciplina implements Comparable<Disciplina>, Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     String nome;
     Double nota;
     String comentario;
