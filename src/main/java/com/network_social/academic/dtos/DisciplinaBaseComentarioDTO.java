0412cbc3a6f29d3da27d2aa8a2e66e6d74c4b4a6
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-13 16:39:58 -0300

    criar dto de comentario

---
+++ src/main/java/com/network_social/academic/dtos/DisciplinaBaseComentarioDTO.java
@@ -0,0 +1,45 @@
+package com.network_social.academic.dtos;
+
+import java.io.Serializable;
+
+import com.network_social.academic.models.Disciplina;
+
+public class DisciplinaBaseComentarioDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String comentario;
+
+    public DisciplinaBaseComentarioDTO(Disciplina disciplina) {
+        this.id = disciplina.getId();
+        this.nome = disciplina.getNome();
+        this.comentario = disciplina.getComentario();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+}
